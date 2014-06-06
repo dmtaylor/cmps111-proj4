43d4c7ff831fbaaf2133131d0dc2979ba77dabbc
David Taylor <dmtaylor2011@gmail.com>
2014-06-06 15:46:32 -0700

    Wrote metacat/tag programs.
    Need to verify

--- utils/metacat.c
+++ utils/metacat.c
@@ -5,4 +5,28 @@
  * 
  * */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#DEFINE MAX_SIZE 1024
+
+
+int main(int argc, char** argv){
+    int fd;
+    char buffer[MAX_SIZE];
+    
+    
+    if(argc != 2){
+        fprintf(stderr, "Usage: metacat FILE\n");
+        exit(1);
+    }
+    fd = open(argv[1], O_RDWR);
+    if(fd == -1){
+        fprintf(stderr, "Error, file not found\n");
+        exit(-1);
+    }
+    metaread(fd, &buffer, MAX_SIZE);
+    printf("%s\n", buffer);
+    return 0;   
+}
--- utils/metatag.c
+++ utils/metatag.c
@@ -4,5 +4,31 @@
  * 
  * */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
+int main(int argc, char** argv){
+    char* buffer;
+    int fd, i, len;
+    
+    if(argc < 3){
+        fprintf(stderr, "Usage: metatag FILE TAG\n");
+        exit(1);
+    }
+    fd=open(argv[1], O_RDWR);
+    if(fd == -1){
+        fprintf(stderr, "Error, file not found\n");
+        exit(1);
+    }
+    buffer = "";
+    for(i=2; i<argc; i++){
+        strcat(buffer, argv[i]);
+        strcat(buffer, " ");
+    }
+    len = strlen(buffer);
+    metawrite(fd,&buffer,len);
+    return 0;
+    
+}
