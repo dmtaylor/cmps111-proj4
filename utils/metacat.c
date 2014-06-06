a25a819cd918782a41adf3d69bcccfb7ee182094
David Taylor <dmtaylor2011@gmail.com>
2014-06-06 15:20:39 -0700

    Init commit for utils

---
+++ utils/metacat.c
@@ -0,0 +1,8 @@
+/* metacat.c: Program for printing metadata from file
+ * By: Forrest Kerslager, Nick Noto, David Taylor, Kevin Yeap,
+ *     Connie Yu
+ * 
+ * 
+ * */
+
+
---
+++ utils/metatag.c
@@ -0,0 +1,8 @@
+/* metatag.c: Program for adding metadata to a file
+ * By: Forrest Kerslager, Nick Noto, David Taylor, Kevin Yeap,
+ *     Connie Yu
+ * 
+ * */
+
+
+
