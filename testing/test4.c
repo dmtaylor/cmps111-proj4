4af8e70f77d402c523255ec3e9b92584bd35ad46
nnoto <nnoto@ucsc.edu>
2014-06-07 18:13:57 -0700

    Test 4 fixed and working

--- testing/test4.c
+++ testing/test4.c
@@ -1,4 +1,6 @@
 /*Demonstrate that changing the metadata does not change the regular file contents*/
+/*Should display "meta meta meta" as the altered metadata content, followed by 
+"Regular file contents", which means the regular file contents weren't changed */
 #include <lib.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -11,7 +13,7 @@
 
 int main (void)
 {
-	int fd, i = 0;
+	int fd;
     /*buffer for regular file contents*/
     char bufferReg[BUF_SIZE] = "Regular file contents\n";
     /*buffer for metadata region contents*/
@@ -24,39 +26,37 @@ int main (void)
 
     /*write to regular file region*/
     write(fd, &bufferReg, BUF_SIZE-1);
+    sync();
     /*clear buffer*/
     /* Want to clear whole buffer, not the size of a pointer d.t. */
     memset(&bufferReg, 0, sizeof(char)*BUF_SIZE); 
-    
-
+    close(fd);
+    fd = open("testfile.txt", O_RDWR | O_CREAT, S_IRWXU);
+	if (fd == -1) { 
+		printf("DEBUG: File open failed.\n");
+	}
     /*write metadata*/
     metawrite(fd, &buffer, BUF_SIZE-1);
+    sync();
     /*clear buffer*/
     /* Want to clear whole buffer, not the size of a pointer d.t. */
     memset(&buffer, 0, sizeof(char)*BUF_SIZE);
     /*confirm metadata written*/
     metaread(fd,&buffer,BUF_SIZE-1);
     printf("%s\n", buffer);
-    
-    /* Not sure if the following code is necessary*/
-    /*while(buffer[i] != '\0') {
-        printf("%c", buffer[i]);
-        i++;
-    }*/
-    i = 0;
+
     /*clear buffer*/
     memset(&buffer, 0, sizeof(char)*BUF_SIZE);
-
+    close(fd);
+    fd = open("testfile.txt", O_RDWR | O_CREAT, S_IRWXU);
+	if (fd == -1) { 
+		printf("DEBUG: File open failed.\n");
+	}
     /*check that regular file contents unchanged*/
     read(fd, &bufferReg, BUF_SIZE-1);
-    
+    close(fd);
     printf("%s\n", bufferReg);
 
-    /*while(bufferReg[i] != '\0') {
-        printf("%c", bufferReg[i]);
-        i++;
-    }*/
-    i = 0;
 	printf("\n\n");
 
 }
